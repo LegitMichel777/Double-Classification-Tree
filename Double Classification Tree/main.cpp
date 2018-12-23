220d0fe758ea403f528de86eb7c4777aa995bf4a
LegitMichel777 <legitmichel777@legitmichel777s-imac.local>
2018-12-23 09:21:13 +0800

    Initial Commit

---
+++ Double Classification Tree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Double Classification Tree
+//
+//  Created by LegitMichel777 on 12/23/18.
+//  Copyright © 2018 LegitMichel777. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
